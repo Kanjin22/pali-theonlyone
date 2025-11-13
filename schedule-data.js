5907d18f89f7be02a7fdfa9ea1750b15326a34aa
Kanjin22 <setthachayo@gmail.com>
2025-11-13 11:50:03 +0700

    50-51

--- schedule-data.js
+++ schedule-data.js
@@ -20,9 +20,9 @@ const scheduleData = [
     date: "2025-11-12",
     displayDate: "พ. ๑๒-พ.ย.-๒๕",
     notes: "อ่านหนังสือ",
-    fileNote: "materials/grammar-notes-50-51", // ใส่ "materials/ชื่อไฟล์.pdf" เมื่อมีไฟล์
+    fileNote: "materials/grammar-notes-50-51.pdf", // ใส่ "materials/ชื่อไฟล์.pdf" เมื่อมีไฟล์
     examTime: "18:30:00",
-    fileExam: "exams/grammar-exam-50-51", // ใส่ "exams/ชื่อไฟล์.pdf" เมื่อมีไฟล์
+    fileExam: "exams/grammar-exam-50-51.pdf", // ใส่ "exams/ชื่อไฟล์.pdf" เมื่อมีไฟล์
     remarks: "ข้อ ๑-๒ (สมัญญาภิธาน-สนธิ) ปี' ๕๐-๕๑"
   },
   {
