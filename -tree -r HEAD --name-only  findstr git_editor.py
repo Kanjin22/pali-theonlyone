b04ed51dcd10c096dca843a17eebb5c4721ed414
Kanjin22 <setthachayo@gmail.com>
2025-12-30 06:29:30 +0700

    new file:   "-tree -r HEAD --name-only \357\201\274 findstr git_editor.py" modified:   .vscode/PythonImportHelper-v2-Completion.json modified:   data/vocab-tananunto.js deleted:    git_editor.py

---
+++ -tree -r HEAD --name-only  findstr git_editor.py
@@ -0,0 +1,5 @@
+[33md3e50e4[m[33m ([m[1;36mHEAD[m[33m -> [m[1;32mmain[m[33m)[m modified:   .vscode/PythonImportHelper-v2-Completion.json new file:   git_editor.py modified:   presentation.html
+[33mb949d6f[m feat(roots): migrate roots data to firebase with caching
+[33m71dc3be[m สวส
+[33ma005169[m feat(roots): migrate roots data to firebase with caching
+[33mbdf3de0[m refactor: remove unused files and update vocab processing scripts
