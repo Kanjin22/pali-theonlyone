bcd80dbaf768db7be6865da0c27adfd0dcf93ddc
Kanjin22 <setthachayo@gmail.com>
2026-01-30 20:42:18 +0700

    chore: restore firebase client config for github pages

---
+++ config.js
@@ -0,0 +1,9 @@
+window.__FIREBASE_CONFIG = {
+  apiKey: "AIzaSyC3ib32Tk9p40p2Z2j30Yogxy0lR8vSM28",
+  authDomain: "palitest-generator.firebaseapp.com",
+  projectId: "palitest-generator",
+  storageBucket: "palitest-generator.firebasestorage.app",
+  messagingSenderId: "844040146831",
+  appId: "1:844040146831:web:b19c0a8a5493299f6ec5fa",
+  measurementId: "G-RKML6H6EX7"
+};
