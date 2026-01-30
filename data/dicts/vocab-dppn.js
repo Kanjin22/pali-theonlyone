1dda7d97d3f3d79e390d36fcdcd0cc65e36e8054
Kanjin22 <setthachayo@gmail.com>
2026-01-30 18:57:49 +0700

    chore(security): redact remaining API keys in docs

--- data/dicts/vocab-dppn.js
+++ data/dicts/vocab-dppn.js
@@ -1,15 +1,1 @@
-(function() {
-  try {
-    var s = document.createElement('script');
-    s.src = '../data/vocab-dppn.js';
-    s.async = false;
-    var current = document.currentScript;
-    if (current && current.parentNode) {
-      current.parentNode.insertBefore(s, current.nextSibling);
-    } else {
-      document.head.appendChild(s);
-    }
-  } catch (e) {
-    console.error('Failed to load vocab-dppn.js from original path:', e);
-  }
 })();
